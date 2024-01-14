1df9774567b2146a6ecf393c909e0cf6b491c809
HarshanaWAJ <janithharshana2019@gmail.com>
2024-01-14 12:14:49 +0530

    backend init

---
+++ backend/src/test/java/com/jc_solusions/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc_solusions.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

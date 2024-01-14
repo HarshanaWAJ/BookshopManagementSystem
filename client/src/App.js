e7ca5054641ad799264f8483fc19f758c70659ab
HarshanaWAJ <janithharshana2019@gmail.com>
2024-01-14 18:10:12 +0530

    front end appBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Appbar from '../src/components/Appbar.jsx';
 
 function App() {
   return (
     <div className="App">
-      Janith Harshana
+      <Appbar/>
     </div>
   );
 }
